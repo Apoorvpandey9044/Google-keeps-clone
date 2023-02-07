540cd3b140cd73cf50280cc96c488c056ed0c40a
Apoorvpandey9044 <pandeyapoorv6@gmail.com>
2023-02-08 00:32:18 +0530

    First commit

---
+++ src/components/notes.js
@@ -0,0 +1,25 @@
+const notes = [
+    {
+        key:1,
+        title: "Delegation",
+        content:
+        "Q. How do you want to"
+    },
+    {
+        key:2,
+        title: "Loops",
+        content: "Hows the josh"
+    },
+    {
+        key:3,
+        title: "Arrays",
+        content: "Hello"
+    },
+    {
+        key:4,
+        title: "Hradware and Software",
+        content: "Hey!!"
+    }
+];
+
+export default notes;
